da496b59c4fbb5c5ac2b35917d4db63c174a3f55
Kromatic97 <rogiromero@gmail.com>
2022-07-14 13:26:05 -0400

    modificacion ecomerce

--- main.js
+++ main.js
@@ -27,6 +27,7 @@ const arrayProducts = [
     
   ]
 
+
 const sales = {};
 
 document.addEventListener("DOMContentLoaded", function(){
@@ -78,9 +79,82 @@ document.addEventListener("DOMContentLoaded", function(){
 
 
 
+//en clase se le llamo---> printFood()
+
+
+
+
+// const contentProducto=document.querySelector(".products_content-grid");
+
+
+// contentProducto.addEventListener("click",(event)=>{
+//     console.log(event.target);
+// })
+
+
 
 function agregarCardsProducts() {
-    const products=document.querySelector(".products_content-grid");
+
+const products=document.querySelector(".products_content-grid");
+
+const shoppingObj = {};
+
+products.addEventListener("click",(event)=>{
+        if(event.target.classList.contains('products_button')){
+            const idProduct2=parseInt(event.target.parentElement.dataset.idproduct);
+            
+
+            let currentProduct2=null;
+
+            for (let i = 0; i < arrayProducts.length; i++) {
+               if (arrayProducts[i].id=== parseInt(idProduct2)) {
+                    currentProduct2=arrayProducts[i];  
+                    
+                }   
+            }
+
+           if (shoppingObj[currentProduct2.id]){
+            shoppingObj[currentProduct2.id].amount++;
+           }else{
+            shoppingObj[currentProduct2.id]=currentProduct2;
+            shoppingObj[currentProduct2.id].amount=1;
+
+           }
+          
+           const shoppingArray= Object.values(shoppingObj)
+           const contentShoping=document.querySelector(".content_shoping")
+           let html2='';
+
+           shoppingArray.forEach(({id, name, price, image,quantity, amount})=>{
+                    html2+=`<div class="shopping">
+
+                            <div class="shopping_header">
+                                <div class="shopping_img">
+                                    <img src="${image}" alt="${name}">
+                                </div>
+                                <div class="shoping_info">
+                                    <p>Nombre: ${name}</p>
+                                    <p>Precio${price}</p>
+                                    <p>Stock${quantity}</p>
+                                </div>
+
+                            </div>
+
+                            <div class="shopping_action" id="${id}>
+                                <span class="rest">-</span>
+                                <b class="amount">${amount}</b>
+                                <span class="add">+</span>
+                                <span class="del">
+                                    <i class='bx bxs-trash' ></i>
+                                </span>
+                            </div>
+                        </div>`
+           })
+           contentShoping.innerHTML= html2;
+        }
+    });
+
+
 
 let html = "";
 
@@ -105,8 +179,8 @@ products.innerHTML=html;
 }
 
 function openAndCloseCards() {
-    const sidebarCard=document.querySelector(".sidebar_card");
-    const navMenuBoton=document.querySelector(".box_sale");
+    const sidebarCard=document.querySelector(".sidebar_card");//card_shopping
+    const navMenuBoton=document.querySelector(".box_sale");//container_shpopping
     navMenuBoton.addEventListener("click", function(){
         sidebarCard.classList.toggle('show_sidebar_card')
     })
